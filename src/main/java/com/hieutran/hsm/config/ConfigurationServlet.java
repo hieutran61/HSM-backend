d77c147261dbeae1c3b3c16deb12218e9d657421
Hieu Tran <hieutran6102@gmail.com>
2023-03-22 12:28:13 +0700

    config CORS

--- src/main/java/com/hieutran/hsm/config/ConfigurationServlet.java
+++ src/main/java/com/hieutran/hsm/config/ConfigurationServlet.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -25,6 +26,11 @@ class ConfigurationServlet implements WebMvcConfigurer{
 //        
 //        return viewResolver;
 //    }
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
     
     @Bean
     public DataSource dataSource() {
